870c4150572dd2f7dd2e633d14fd7f898cc75414
Sourav <souravsahu70047@gmail.com>
2023-04-20 19:32:44 +0530

    Added high score logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hoo")
+const playboard=document.querySelector(".play-board");
+const scoreElement=document.querySelector(".score");
+const highScoreElement=document.querySelector(".high-score");
+const controls=document.querySelector(".controls i");
+
+let gameOver=false;
+let foodX,foodY;
+let snakeX=5,snakeY=5;
+let velocityX=0,velocityY=0;
+let snakeBoard=[];
+let id;
+let score;
+
+// Getting highest score from local storage;
+
+let highScore=JSON.parse(localStorage.getItem("high-score"))||0;
+highScoreElement.innerText=`High Score: ${highScore}`
